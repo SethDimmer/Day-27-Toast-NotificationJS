ba3a036dfb35755c089f99589cc37eecfa918ff1
sethdimmer234 <sethdimmer1@gmail.com>
2024-05-27 16:47:22 -0400

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const button = document.getElementById('button')
 const toasts = document.getElementById('toasts')
 
+
+// an array of random messages
+const messages = [
+    'Message One',
+    'Message Two',
+    'Message Three',
+    'Message Four',
+]
+
+
+button.addEventListener('click', () => createNotification())
+// each time i click i want to run the function createNotification()
+
+
+
+
